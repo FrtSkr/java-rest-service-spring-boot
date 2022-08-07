14fb7a160d71f5216b70ebd7a9abe4361e51b121
Fırat Şakar <frtskr0@gmail.com>
2022-08-07 14:10:25 +0300

    started

---
+++ src/test/java/com/bestpricedemo/productadbackend/ProductAdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestpricedemo.productadbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductAdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
